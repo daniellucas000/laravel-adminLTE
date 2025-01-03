b184df7dc372a3b14b180f3e90a7e1facd81704a
Daniel Lucas <danielluccas000@gmail.com>
2025-01-02 22:22:13 -0300

    feat(auth): integrate Fortify and configure authentication system
    - Added Laravel Fortify for user authentication.
    - Configured Fortify settings and published its configuration.
    - Created layout for authentication views.
    - Added pages for user login and registration.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,10 +4,9 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>AdminLTE v4 | Dashboard</title>
-    <!--begin::Primary Meta Tags-->
+    <title>AdminLTE 4 | Register Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="title" content="AdminLTE v4 | Dashboard" />
+    <meta name="title" content="AdminLTE 4 | Register Page" />
     <meta name="author" content="ColorlibHQ" />
     <meta
         name="description"
@@ -18,20 +17,8 @@
     @vite('resources/scss/app.scss')
 </head>
 
-<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
-    <div class="app-wrapper">
-        @include('parts.header')
-        @include('parts.sidebar')
-        <main class="app-main">
-            @include('parts.content-header')
-            <div class="app-content">
-                <div class="container-fluid">
-                    @yield('content')
-                </div>
-            </div>
-        </main>
-        @include('parts.footer')
-    </div>
+<body class="@yield('body-class') bg-body-secondary">
+    @yield('content')
     @vite('resources/js/app.js')
 </body>
 
